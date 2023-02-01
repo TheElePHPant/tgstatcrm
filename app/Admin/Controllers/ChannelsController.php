11cb09bdfa027ba001f0c7cb98a28c651b652320
serg.serdjuk@gmail.com <serg.serdjuk@gmail.com>
2023-02-01 17:35:06 +0200

    WIP

--- app/Admin/Controllers/ChannelsController.php
+++ app/Admin/Controllers/ChannelsController.php
@@ -45,7 +45,7 @@ class ChannelsController extends AdminController
 
         $grid->column('daily_consumption', 'Расход (день)');
         $grid->column('daily_profit', 'Доход (день)');
-        $grid->column('consumptions_sum_amount', 'Расход(всего)');
+        $grid->column('consumptions_sum_amount', 'Расход(всего)')->display(fn()=>$this->consumptions_sum_amount??0);
         $grid->column('profit_sum_amount', 'Доход(всего)');
         $grid->column('daily_subscribers.created_at', 'Дата обновления')->datetime('d.m.Y H:i:s');
         $grid->actions(function ($actions) {
