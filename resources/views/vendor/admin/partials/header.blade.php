2045f9475803a74f600d0e938a44808bc8a8f43c
serg.serdjuk@gmail.com <serg.serdjuk@gmail.com>
2023-02-13 10:05:30 +0200

    2fa

--- resources/views/vendor/admin/partials/header.blade.php
+++ resources/views/vendor/admin/partials/header.blade.php
@@ -53,7 +53,7 @@
                             </div>
                         </li>
                     </ul>
-                </li>
+                </li><!---->
                 <!-- Control Sidebar Toggle Button --><!---->
                 {{--<li>--}}
                 {{--<a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>--}}
